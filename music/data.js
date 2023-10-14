2bcc3e46771ea01d4ccf7e66607eadbde1b24259
kazeem Samod <ayokazeem60@gmail.com>
2023-10-15 07:37:21 +0900

    new work
    imole

---
+++ music/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+       name: 'My Brother',
+       path: 'song/Bella_Shmurda_-_My_Brother_Tribute_To_Mohbad_.mp3',
+       artist: 'Bella_Shmurda',
+       cover: '1057049222.jpg'
+    },
+    {
+        name: 'Peace',
+        path: 'song/Mohbad-Peace-(TrendyBeatz.com).mp3',
+        artist: 'Mohbad',
+        cover: '-516602578.jpg'
+    },
+    {
+        name: 'Blessing',
+        path: 'song/Mohbad_-_Blessing.mp3',
+        artist: 'Mohbad',
+        cover: '1057049222.jpg'
+    },
+    {
+        name: 'La_pio_pio',
+        path: 'song/Mohbad_-_La_Pio_Pio.mp3',
+        artist: 'Mohbad',
+        cover:   '1298648134.jpg'
+    },
+    {
+        name: 'Account_Balance',
+        path: 'song/Mohbad_Ft_Zlatan_-_Account_Balance.mp3',
+        artist: 'Mohbad_ft_Zlatan',
+        cover:   '1737907639.jpg'
+    }
+]
